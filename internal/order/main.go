0f09b18a2a3f3efc846a9a8c87afa03a443083f4
jialechen7 <chenjiale0303@gmail.com>
2025-06-15 15:14:30 +0800

    feat: 搭建http和grpc服务

--- internal/order/main.go
+++ internal/order/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/jialechen7/gorder-v2/common/config"
+	"github.com/jialechen7/gorder-v2/common/genproto/orderpb"
+	"github.com/jialechen7/gorder-v2/common/server"
+	"github.com/jialechen7/gorder-v2/order/ports"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 func init() {
@@ -14,5 +19,19 @@ func init() {
 }
 
 func main() {
-	log.Printf("%v", viper.Get("order"))
+	serviceName := viper.GetString("order.service-name")
+
+	go server.RunGRPCServer(serviceName, func(server *grpc.Server) {
+		svc := ports.NewGRPCServer()
+		orderpb.RegisterOrderServiceServer(server, svc)
+	})
+
+	server.RunHTTPServer(serviceName, func(router *gin.Engine) {
+		svc := ports.NewHTTPServer()
+		ports.RegisterHandlersWithOptions(router, svc, ports.GinServerOptions{
+			BaseURL:      "/api",
+			Middlewares:  nil,
+			ErrorHandler: nil,
+		})
+	})
 }
---
+++ internal/stock/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jialechen7/gorder-v2/common/config"
+	"github.com/jialechen7/gorder-v2/common/genproto/stockpb"
+	"github.com/jialechen7/gorder-v2/common/server"
+	"github.com/jialechen7/gorder-v2/stock/ports"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+)
+
+func init() {
+	if err := config.NewViperConfig(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	serviceName := viper.GetString("stock.service-name")
+	serverType := viper.GetString("stock.server-to-run")
+	switch serverType {
+	case "grpc":
+		server.RunGRPCServer(serviceName, func(server *grpc.Server) {
+			svc := ports.NewGRPCServer()
+			stockpb.RegisterStockServiceServer(server, svc)
+		})
+	case "http":
+		// TODO: 暂时不用
+	default:
+		panic("unexpected server type")
+	}
+}
